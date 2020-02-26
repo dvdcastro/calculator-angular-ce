9094ca617f9f1258c86b20e9eb5a79e63ee28120
David Castro <davidcastro00@gmail.com>
2020-02-26 09:01:11 -0500

    Unit tests for calculator service.

--- src/app/calculator.service.spec.ts
+++ src/app/calculator.service.spec.ts
@@ -13,4 +13,97 @@ describe('CalculatorService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('#calculate should return empty value when using AC',
+    (done: DoneFn) => {
+      service.calculate('AC').subscribe(value => {
+        expect(value).toBe('');
+        done();
+      });
+    });
+
+  it('#calculate should return correct number concatenation numbers and periods',
+    (done: DoneFn) => {
+      service.calculate('1').subscribe(value => {
+        expect(value).toBe('1');
+        service.calculate('2').subscribe(value2 => {
+          expect(value2).toBe('12');
+          service.calculate('.').subscribe(value3 => {
+            expect(value3).toBe('12.');
+            service.calculate('3').subscribe(value4 => {
+              expect(value4).toBe('12.3');
+              done();
+            });
+          });
+        });
+      });
+    });
+
+  it('#calculate should return a sum',
+    (done: DoneFn) => {
+      service.calculate('1').subscribe(value => {
+        expect(value).toBe('1');
+        service.calculate('+').subscribe(value2 => {
+          expect(value2).toBe('1');
+          service.calculate('2').subscribe(value3 => {
+            expect(value3).toBe('2');
+            service.calculate('=').subscribe(value4 => {
+              expect(value4).toBe('3');
+              done();
+            });
+          });
+        });
+      });
+    });
+
+  it('#calculate should return a substraction',
+    (done: DoneFn) => {
+      service.calculate('3').subscribe(value => {
+        expect(value).toBe('3');
+        service.calculate('-').subscribe(value2 => {
+          expect(value2).toBe('3');
+          service.calculate('2').subscribe(value3 => {
+            expect(value3).toBe('2');
+            service.calculate('=').subscribe(value4 => {
+              expect(value4).toBe('1');
+              done();
+            });
+          });
+        });
+      });
+    });
+
+  it('#calculate should return a multiplication',
+    (done: DoneFn) => {
+      service.calculate('3').subscribe(value => {
+        expect(value).toBe('3');
+        service.calculate('×').subscribe(value2 => {
+          expect(value2).toBe('3');
+          service.calculate('2').subscribe(value3 => {
+            expect(value3).toBe('2');
+            service.calculate('=').subscribe(value4 => {
+              expect(value4).toBe('6');
+              done();
+            });
+          });
+        });
+      });
+    });
+
+  it('#calculate should return a division',
+    (done: DoneFn) => {
+      service.calculate('3').subscribe(value => {
+        expect(value).toBe('3');
+        service.calculate('÷').subscribe(value2 => {
+          expect(value2).toBe('3');
+          service.calculate('2').subscribe(value3 => {
+            expect(value3).toBe('2');
+            service.calculate('=').subscribe(value4 => {
+              expect(value4).toBe('1.5');
+              done();
+            });
+          });
+        });
+      });
+    });
 });
